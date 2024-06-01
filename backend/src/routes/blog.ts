d700a484cb0943c1be428adfcda5678a4705874d
Sourav <souravmohanta4428@gmail.com>
2024-06-01 12:22:03 +0530

    add authentication

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,41 +1,64 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { Context, Hono } from "hono";
+import { Hono } from "hono";
+import { verify } from "hono/jwt";
 
 export const blogRouter=new Hono<{
     Bindings:{
         DATABASE_URL:string,
-
+        JWT_SECRET:string
+    },
+    Variables:{
+        userId:string;
     }
 }>();
 
-blogRouter.use("/*",(c,next)=>{
-    //extract the user id
-    //pass it down to the route handler
-    next();
+//AUTH VERICATION ENDPOINT
+blogRouter.use("/*",async(c,next)=>{
+    const authHeader=c.req.header("authorization")||"";
+    const user=await verify(authHeader,c.env.JWT_SECRET);//it must be verify not decode as decoding can be done by anyone but verify should be done by that the user only
+    if(user){
+        c.set("userId",user.id);
+        await next();//it will transfer the user to next route if the token is correct
+    }
+    else{
+        c.status(403);
+        return c.json({
+            message:"you are not logged in"
+        })
+    }
+   await next();
 })
 
-blogRouter.post('/api/v1/blog', async(c) => {
+//FOR POSTING OR CREATRING A NEW END POINT
+blogRouter.post('/', async(c) => {
     const body=await c.req.json();
+    const userId=c.get("userId");
     const prisma=new PrismaClient({
         datasourceUrl:c.env.DATABASE_URL,
     }).$extends(withAccelerate())
 
-    const blog=await prisma.blog.create({
+    try {
+      const blog=await prisma.blog.create({
         data:{
             title:body.title,
             content:body.content,
-            authorId:1//came from blog router after authentication
+            authorId:3//came from blog router after authentication
         }
     })
     
     return c.json({
         id:blog.id
-    })
+    })  
+    } catch (error) {
+        console.log(error)
+    }
+    
 })
   
+//END POINT FOR UPDATING BLOG
 //put router needs to update title and content
-blogRouter.put('/api/v1/blog',async (c) => {
+blogRouter.put('/',async (c) => {
     const body=await c.req.json();
     const prisma=new PrismaClient({
         datasourceUrl:c.env.DATABASE_URL
@@ -56,7 +79,8 @@ blogRouter.put('/api/v1/blog',async (c) => {
   })
 })
 
-blogRouter.get('/api/v1/blog', async(c) => {
+//END POINT FOR GETTING A SINGLE PARTICULAR BLOG
+blogRouter.get('/', async(c) => {
     const body=await c.req.json();
     const prisma=new PrismaClient({
         datasourceUrl:c.env.DATABASE_URL,
@@ -81,6 +105,7 @@ blogRouter.get('/api/v1/blog', async(c) => {
 
 })
   
+//END POINT FOR GETTING ALL THE BLOGS
 blogRouter.get('/bulk', async(c) => {
    const prisma=new PrismaClient({
     datasourceUrl:c.env.DATABASE_URL,
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { decode,sign,verify } from 'hono/jwt'
+import { sign } from 'hono/jwt'
 
 
 export const userRouter=new Hono<{
