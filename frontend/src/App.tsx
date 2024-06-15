3055b90311233c52e250f2675c31ed630d05629c
Sourav <souravmohanta4428@gmail.com>
2024-06-16 00:51:31 +0530

    adding home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,14 @@ import { Signin } from "./pages/Signin";
 import { Signup } from "./pages/Signup";
 import { Blogs } from "./pages/Blogs";
 import { Publish } from "./pages/Publish";
+import { Home } from "./pages/Home";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home/>} />
         <Route path="/signin" element={<Signin/>}/>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/blog/:id" element={<Blog/>}/>
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,68 @@
+import { Link} from "react-router-dom";
+import home from "../../src/Image/home.png"
+
+export const Home=()=>{
+    return( <div>
+            <div className="flex justify-between border-b px-10 py-4">
+               <div className="font-serif flex flex-col justify-center font-semibold text-4xl text px-36">
+                   Medium
+               </div>
+                <div className="font-serif flex space-x-5 px-28 text-md font-normal">
+
+                    <Link to={("/signup")}> Write</Link>
+                    <Link to={("/signin")}>Sign In</Link>
+                    <Link to={("/signup")}>
+                        <button className="bg-black text-white px-3 py-1 rounded-lg">Get Started</button> 
+                    </Link>
+              </div>           
+            </div>
+
+            <div className="flex border-b">
+             <div className="flex flex-col justify-center  px-10 py-10">
+                   <div className="px-28 text-8xl font-semibold pt-14"> 
+                       Human 
+                   </div>
+                   <div className="px-28 text-8xl font-semibold">
+                       stories & ideas
+                   </div>
+                   <div className="px-28 text-xl font-semibold pt-7">
+                       A place to read,write,and deepen your understanding
+                   </div>
+                   <div className="px-28 text-xl font-normal pt-7">
+                        <button className="bg-black text-white px-6 py-2 rounded-2xl">Start Reading</button>
+                   </div>
+               </div>
+               <div className="vissible">
+                   <img src={home} alt="" />
+               </div>
+             </div>
+
+            <div className="flex justify-center space-x-5 pt-5 pb-5 text-slate-500">
+                <div>Help</div>
+                <div>Status</div>
+                <div>About</div>
+                <div>Careers</div>
+                <div>Press</div>
+                <div>Blog</div>
+                <div>Privacy</div>
+                <div>Term</div>
+                <div>Text to speech</div>
+                <div>Teams</div>
+            </div>
+        </div>
+    )
+}
+
+
+        // <div className="flex justify-between border-b px-10 py-4">
+        //     <Link to={"/blogs"} className="flex flex-col justify-center font-bold text-2xl">
+        //         Medium
+        //     </Link>
+        //     <div>
+        //         <Link to={"/publish"}>
+        //         <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">New</button>
+        //         </Link>
+        //         <Avatar name="sourav" size={"big"}/>
+        //     </div>
+        // </div>
+ 
