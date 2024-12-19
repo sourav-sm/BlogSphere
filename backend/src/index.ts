f93525015e83ad38894110f37dfeb1866caf13e3
SOURAV MOHANTA <116539402+sourav-sm@users.noreply.github.com>
2024-12-19 09:40:33 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,11 +24,3 @@ app.get('/', (c) => {
 
 
 export default app
-
-//postgresql://developersourav135:GDWbeuk6ZPv0@ep-royal-bread-48297123.us-east-2.aws.neon.tech/nextjs?sslmode=require
-//the above postgress url will be use in .env file as a DATABASE_URL
-
-
-//DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiZDM4MzVhZjUtNTc3Ni00MTcxLWExYWYtZjRjYjViZDU1YThhIiwidGVuYW50X2lkIjoiNWEwZjJiZGY4OWUyZGMzN2Q4OGQ4ZjkzZWI5MzFhZjJhMjM2NmMyZmUyOTA1ZmRkZTMwMWVjMGQ4MDNkNWY1YSIsImludGVybmFsX3NlY3JldCI6ImRjZmQwYjgxLTI5NWEtNDU1MC1hNjcwLTgzMmUzOTg0ZTU4MiJ9.gU1L55OYGC-sFJ2CyndbkCwJkEcazy3BGgiujyeD4yg"
-
-//the above prisma url will be use in wrangler.toml file as a DATABASE_URL
